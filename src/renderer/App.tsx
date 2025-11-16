0ea0e28adc6db854ddd08b2fab97240af124b4b2
ryee-dao <rdau2010@gmail.com>
2025-11-15 23:39:54 -0500

    Add react

---
+++ src/renderer/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+    return (
+        <div><p>Hello from React</p></div>
+    )
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // Don't need vite for main process, only renderer
 export default defineConfig({
+  plugins: [react()],
   root: path.resolve(__dirname, 'src/renderer'),
   base: './',
   build: {
