8f8c24979ba35b2c4672b166e3aa529faf2e3636
Ryan Dao <rdau2010@gmail.com>
2026-03-09 01:20:47 -0400

    Fix vulnerabilities and fix playwright clash with vitest

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+    testDir: "./tests/e2e",
     globalSetup: "./tests/e2e/global-setup.ts",
     workers: 2,
     reporter: [
